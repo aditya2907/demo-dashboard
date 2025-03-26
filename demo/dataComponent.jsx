20ea2a11b4cc8f9e2f998ab6259d87eda09065fa
Aditya Suryawanshi <50817155+aditya2907@users.noreply.github.com>
2025-03-26 16:35:06 +0530

    Create dataComponent.jsx

---
+++ demo/dataComponent.jsx
@@ -0,0 +1,34 @@
+// src/components/DataComponent.js
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from '../actions/dataActions';
+
+const DataComponent = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector((state) => state.data);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Data List</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataComponent;
